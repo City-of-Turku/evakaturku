079072a304c65f0d06c6a133a7ce79a1002bcf10
tahtee <timo.ahteela@gofore.com>
2022-11-15 10:37:55 +0200

    262-lapsen-esikouluhakemuksen-vastaanotto-sähköposti

--- service/src/main/kotlin/fi/turku/evakaturku/emailclient/config/EmailConfiguration.kt
+++ service/src/main/kotlin/fi/turku/evakaturku/emailclient/config/EmailConfiguration.kt
@@ -121,8 +121,59 @@ internal class EmailMessageProvider(): IEmailMessageProvider {
     override fun getDecisionEmailHtml(childId: ChildId, decisionId: AssistanceNeedDecisionId): String = "X"
     override fun getDecisionEmailText(childId: ChildId, decisionId: AssistanceNeedDecisionId): String = "X"
 
-    override fun getPreschoolApplicationReceivedEmailHtml(withinApplicationPeriod: Boolean): String = "X"
+    override fun getPreschoolApplicationReceivedEmailHtml(withinApplicationPeriod: Boolean): String {
+        return """
+            <p>Hei!</p>
+            
+            <p>Olemme vastaanottaneet lapsenne hakemuksen esiopetukseen. Hakemuksen tehnyt huoltaja voi muokata hakemusta osoitteessa <a href="https://evaka.turku.fi">evaka.turku.fi</a> siihen saakka, kunnes varhaiskasvatuksen palveluohjaus ottaa sen käsittelyyn. Päätös on nähtävissä ja hyväksyttävissä/hylättävissä <a href="https://evaka.turku.fi">evaka.turku.fi</a>.</p>
+            
+            <p>Mikäli ilmoititte hakemuksessa lapsenne tuen tarpeesta, varhaiskasvatuksen erityisopettaja on teihin yhteydessä, jotta lapsen tuen tarpeet voidaan ottaa huomioon paikkaa osoitettaessa.</p>
+            
+            <p>ESIOPETUKSEN TÄYDENTÄVÄ VARHAISKASVATUS</p>
+            
+            <p>Mikäli hait esiopetukseen täydentävää varhaiskasvatusta, otathan huomioon:</p>
+            
+            <p><ul>
+            <li>Vuorohoidon palveluita järjestetään vanhempien vuorotyön tai iltaisin ja/tai viikonloppuisin tapahtuvan opiskelun perusteella.</li>
+            <li>Mikäli lapsellenne järjestyy varhaiskasvatuspaikka jostakin hakemuksessa toivomastanne kunnallisesta varhaiskasvatuspaikasta, ilmoitamme teille paikan viimeistään kaksi viikkoa ennen varhaiskasvatuksen toivottua aloitusajankohtaa.</li>
+            <li>Mikäli valitsitte ensimmäiseksi hakutoiveeksi yksityisen päiväkodin, olkaa suoraan yhteydessä kyseiseen yksikköön varmistaaksenne varhaiskasvatuspaikan saamisen. Mikäli toivomanne palveluntuottaja ei pysty tarjoamaan hoitopaikkaa, pyydämme teitä olemaan yhteydessä varhaiskasvatuksen palveluohjaukseen.</li>
+            <li>Siirtohakemukset (lapsella on jo varhaiskasvatuspaikka Turun kaupungin varhaiskasvatusyksikössä) käsitellään pääsääntöisesti hakemuksen saapumispäivämäärän mukaan.</li>
+            </ul></p>
+            
+            <p>Päätös on nähtävissä ja hyväksyttävissä/hylättävissä evaka.turku.fi</p>
+            
+            <p>Hakemuksen liitteet voi lisätä suoraan sähköiselle hakemukselle tai toimittaa postitse osoitteeseen Varhaiskasvatuksen palveluohjaus, PL 355, 20101 Turku tai toimittamalla Kauppatorin Monitoriin, Varhaiskasvatuksen palveluohjaus, Aurakatu 8.</p>
+            
+            <p>
+            Ystävällisesti,<br/>
+            Varhaiskasvatuksen palveluohjaus<br/>
+            </p>
+        """.trimIndent()
+    }
 
-    override fun getPreschoolApplicationReceivedEmailText(withinApplicationPeriod: Boolean): String = "X"
+    override fun getPreschoolApplicationReceivedEmailText(withinApplicationPeriod: Boolean): String {
+        return """
+            Hei! 
+            
+            Olemme vastaanottaneet lapsenne hakemuksen esiopetukseen. Hakemuksen tehnyt huoltaja voi muokata hakemusta osoitteessa evaka.turku.fi siihen saakka, kunnes varhaiskasvatuksen palveluohjaus ottaa sen käsittelyyn. Päätös on nähtävissä ja hyväksyttävissä/hylättävissä evaka.turku.fi.
+            
+            Mikäli ilmoititte hakemuksessa lapsenne tuen tarpeesta, varhaiskasvatuksen erityisopettaja on teihin yhteydessä, jotta lapsen tuen tarpeet voidaan ottaa huomioon paikkaa osoitettaessa.
+
+            ESIOPETUKSEN TÄYDENTÄVÄ VARHAISKASVATUS 
+
+            Mikäli hait esiopetukseen täydentävää varhaiskasvatusta, otathan huomioon: 
+            •	Vuorohoidon palveluita järjestetään vanhempien vuorotyön tai iltaisin ja/tai viikonloppuisin tapahtuvan opiskelun perusteella. 
+            •	Mikäli lapsellenne järjestyy varhaiskasvatuspaikka jostakin hakemuksessa toivomastanne kunnallisesta varhaiskasvatuspaikasta, ilmoitamme teille paikan viimeistään kaksi viikkoa ennen varhaiskasvatuksen toivottua aloitusajankohtaa.
+            •	Mikäli valitsitte ensimmäiseksi hakutoiveeksi yksityisen päiväkodin, olkaa suoraan yhteydessä kyseiseen yksikköön varmistaaksenne varhaiskasvatuspaikan saamisen. Mikäli toivomanne palveluntuottaja ei pysty tarjoamaan hoitopaikkaa, pyydämme teitä olemaan yhteydessä varhaiskasvatuksen palveluohjaukseen. 
+            •	Siirtohakemukset (lapsella on jo varhaiskasvatuspaikka Turun kaupungin varhaiskasvatusyksikössä) käsitellään pääsääntöisesti hakemuksen saapumispäivämäärän mukaan.
+             
+            Päätös on nähtävissä ja hyväksyttävissä/hylättävissä evaka.turku.fi
+             
+            Hakemuksen liitteet voi lisätä suoraan sähköiselle hakemukselle tai toimittaa postitse osoitteeseen Varhaiskasvatuksen palveluohjaus, PL 355, 20101 Turku tai toimittamalla Kauppatorin Monitoriin, Varhaiskasvatuksen palveluohjaus, Aurakatu 8.
+
+            Ystävällisesti, 
+            Varhaiskasvatuksen palveluohjaus 
+        """.trimIndent()
+    }
 
 }
