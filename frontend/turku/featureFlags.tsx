b357a8583fa094b8f4e741ff682fefe09da47453
Mikael Kulma <mikael.kulma@kipina.fi>
2022-11-16 15:39:09 +0200

    Update frontend featureflags, enable setting per env

--- frontend/turku/featureFlags.tsx
+++ frontend/turku/featureFlags.tsx
@@ -8,28 +8,74 @@ SPDX-License-Identifier: LGPL-2.1-or-later
 
 import { FeatureFlags } from 'lib-customizations/types'
 
-const featureFlags: FeatureFlags = {
-  citizenShiftCareAbsenceEnabled: true,
-  daycareApplication: {
-    dailyTimesEnabled: true
-  },
-  groupsTableServiceNeedsEnabled: true,
-  evakaLogin: true,
-  financeBasicsPage: true,
-  urgencyAttachmentsEnabled: true,
-  preschoolEnabled: true,
-  assistanceActionOtherEnabled: true,
-  experimental: {
-    messageAttachments: true,
-    personalDetailsPage: true,
-    voucherUnitPayments: true,
-    assistanceNeedDecisions: true,
-    assistanceNeedDecisionsLanguageSelect: false,
-    mobileMessages: true,
-    citizenVasu: true,
-    fosterParents: true
-  },
-  adminSettingsEnabled: true
+import { env, Env } from './env'
+
+type Features = {
+  default: FeatureFlags
+} & {
+  [k in Env]: FeatureFlags
 }
+
+const features: Features = {
+  default: {
+    citizenShiftCareAbsence: true,
+    assistanceActionOther: true,
+    daycareApplication: {
+      dailyTimes: true
+    },
+    groupsTableServiceNeeds: true,
+    preschool: true,
+    urgencyAttachments: true,
+    experimental: {
+      leops: true,
+      citizenVasu: true,
+      voucherUnitPayments: true,
+      assistanceNeedDecisions: true,
+      assistanceNeedDecisionsLanguageSelect: false,
+      staffAttendanceTypes: true,
+      fosterParents: true
+    }
+  },
+  staging: {
+    citizenShiftCareAbsence: true,
+    assistanceActionOther: true,
+    daycareApplication: {
+      dailyTimes: true
+    },
+    groupsTableServiceNeeds: true,
+    preschool: true,
+    urgencyAttachments: true,
+    experimental: {
+      leops: true,
+      citizenVasu: true,
+      voucherUnitPayments: true,
+      assistanceNeedDecisions: true,
+      assistanceNeedDecisionsLanguageSelect: false,
+      staffAttendanceTypes: true,
+      fosterParents: true
+    }
+  },
+  prod: {
+    citizenShiftCareAbsence: true,
+    assistanceActionOther: true,
+    daycareApplication: {
+      dailyTimes: true
+    },
+    groupsTableServiceNeeds: true,
+    preschool: true,
+    urgencyAttachments: true,
+    experimental: {
+      leops: true,
+      citizenVasu: true,
+      voucherUnitPayments: true,
+      assistanceNeedDecisions: true,
+      assistanceNeedDecisionsLanguageSelect: false,
+      staffAttendanceTypes: true,
+      fosterParents: true
+    }
+  }
+}
+
+const featureFlags = features[env()]
 
 export default featureFlags
