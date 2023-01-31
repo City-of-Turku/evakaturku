a789dc9afae8c1cf42c7a839b6a346112f1c833b
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2023-01-31 11:01:27 +0200

    XML invoices work in progress

---
+++ service/src/test/kotlin/fi/turku/evakaturku/invoice/service/InvoiceSenderTest.kt
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2021 City of Oulu
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package fi.ouka.evakaoulu.invoice.service
+
+import fi.ouka.evakaoulu.SftpProperties
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.*
+
+internal class InvoiceSenderTest {
+
+    @Test
+    fun `should send invoice`() {
+        val path = "/some/path"
+        val sftpProperties = SftpProperties("", path, "", "")
+        val proEInvoice = "one"
+        val sftpConnector = mock<SftpConnector>()
+        val sftpSender = SftpSender(
+            sftpProperties, sftpConnector
+        )
+
+        sftpSender.send(proEInvoice)
+
+        verify(sftpConnector).connect(sftpProperties.address, sftpProperties.username, sftpProperties.password)
+        val fileNamePattern = """$path/proe-\d{8}-\d{6}.txt"""
+        verify(sftpConnector).send(
+            argThat { filePath -> filePath.matches(Regex(fileNamePattern)) },
+            eq("one")
+        )
+        verify(sftpConnector).disconnect()
+    }
+}
---
+++ service/src/test/kotlin/fi/turku/evakaturku/invoice/service/ProEInvoiceGeneratorTest.kt
@@ -0,0 +1,82 @@
+package fi.ouka.evakaoulu.invoice.service
+
+import fi.espoo.evaka.invoicing.domain.InvoiceDetailed
+import fi.ouka.evakaoulu.util.FieldType
+import fi.ouka.evakaoulu.util.FinanceDateProvider
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+
+internal class ProEInvoiceGeneratorTest {
+
+    val financeDateProvider = mock<FinanceDateProvider>()
+    val proEInvoiceGenerator = ProEInvoiceGenerator(InvoiceChecker(), financeDateProvider)
+
+    @BeforeEach
+    fun setup() {
+        whenever(financeDateProvider.currentDate()).thenReturn("20220505")
+        whenever(financeDateProvider.previousMonth()).thenReturn("04.2022")
+    }
+
+    @Test
+    fun `should return successfully created invoices in success list`() {
+
+        val invoice = validInvoice()
+        val invoiceList = listOf(invoice, invoice)
+
+        val generationResult = proEInvoiceGenerator.generateInvoice(invoiceList)
+        assertEquals(generationResult.sendResult.succeeded, invoiceList)
+        assertEquals(generationResult.sendResult.manuallySent, listOf<InvoiceDetailed>())
+        assertEquals(generationResult.sendResult.failed, listOf<InvoiceDetailed>())
+    }
+
+    @Test
+    fun `should return manually sent invoices in manually list`() {
+
+        val restrictedInvoice = validInvoice().copy(headOfFamily = personWithRestrictedDetails())
+        val invoiceWithoutSsn = validInvoice().copy(headOfFamily = personWithoutSSN())
+        val invoiceList = listOf(restrictedInvoice, invoiceWithoutSsn)
+
+        val generationResult = proEInvoiceGenerator.generateInvoice(invoiceList)
+        assertEquals(generationResult.sendResult.succeeded, listOf<InvoiceDetailed>())
+        assertEquals(generationResult.sendResult.manuallySent, invoiceList)
+        assertEquals(generationResult.sendResult.failed, listOf<InvoiceDetailed>())
+    }
+
+    @Test
+    fun `should format invoice rows according to data and formatting`() {
+
+        val format = listOf(
+                InvoiceField(InvoiceFieldName.INVOICE_IDENTIFIER, FieldType.ALPHANUMERIC, 1, 11),
+                InvoiceField(InvoiceFieldName.CLIENT_NAME1, FieldType.ALPHANUMERIC, 12, 30),
+                InvoiceField(InvoiceFieldName.INCLUDED_LATE_PAYMENT_INTEREST, FieldType.NUMERIC, 42, 6, 2)
+        )
+        val invoiceData = InvoiceData()
+
+        invoiceData.setAlphanumericValue(InvoiceFieldName.INVOICE_IDENTIFIER, "121212A121A")
+        invoiceData.setAlphanumericValue(InvoiceFieldName.CLIENT_NAME1, "Jokunen Jaska")
+        invoiceData.setNumericValue(InvoiceFieldName.INCLUDED_LATE_PAYMENT_INTEREST, 42)
+
+        val result = proEInvoiceGenerator.generateRow(format, invoiceData)
+
+        assertEquals(result, "121212A121AJokunen Jaska                 00004200\n")
+    }
+
+    @Test
+    fun `should check that invoice format is a proper one also with invoice function number`() {
+
+        val invoice = validInvoice()
+        val longNamedInvoice = validInvoice().copy(headOfFamily = personWithLongName())
+        val invoiceList = listOf(invoice, longNamedInvoice)
+
+        val generationResult = proEInvoiceGenerator.generateInvoice(invoiceList)
+
+        var correctInvoice = object {}.javaClass.getResource("/invoice-client/CorrectProEInvoice.txt")?.readText()
+
+        assertEquals(correctInvoice, generationResult.invoiceString)
+    }
+
+}
