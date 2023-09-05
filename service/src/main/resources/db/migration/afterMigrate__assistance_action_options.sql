291786dc01f7172b15e2699b920188d3e14394bb
mrosin <martin.rosin.fi@gmail.com>
2023-09-05 15:07:26 +0300

    renamed turku assistance action option texts

--- service/src/main/resources/db/migration/afterMigrate__assistance_action_options.sql
+++ service/src/main/resources/db/migration/afterMigrate__assistance_action_options.sql
@@ -5,18 +5,14 @@
 INSERT INTO assistance_action_option
     (value, name_fi, description_fi, display_order)
 VALUES
-    ('10', 'Avustajataso I', 'lapsikohtainen', 10),
-    ('20', 'Avustajataso II', '2–3 lapselle', 20),
-    ('30', 'Avustajataso III', '2–5 lapselle', 30),
-    ('40', 'Tuen hoitaja', 'Tuen hoitaja on resurssi, mitä ei lasketa suhdelukuun. Pedagogisesti vahvistettu ryhmä.', 40),
-    ('50', 'Pienennetty ryhmä', 'Ryhmän rakenne: Opettaja, lastenhoitaja ja avustaja tai tuen hoitaja. Lapsiluku 13 kpl, joista kolmella lapsella tuen tarve', 50),
-    ('60', 'Alueellinen integroitu ryhmä', 'Ryhmän rakenne: VEO, opettaja ja kaksi lastenhoitajaa. Lapsiluku 14 kpl, joista viidellä tuen tarve.', 60),
-    ('70', 'Erityisryhmä', 'Sarat-ryhmä. Ryhmän rakenne: VEO, sairaanhoitaja ja kaksi lastenhoitajaa. Lapsiluku 8 kpl, joista kaikilla tuen tarve.', 70),
+    ('10', 'Lapsikohtainen avustaja', NULL, 10),
+    ('20', 'Ryhmäkohtainen avustaja', NULL, 20),
+    ('40', 'Tuen hoitaja', 'TH on resurssi, mitä ei lasketa suhdelukuun. Pedagogisesti vahvistettu ryhmä', 40),
+    ('50', 'Pienennetty ryhmä', 'Ryhmän rakenne: VO, LH ja avustaja tai TH. Lapsiluku 13 kpl, joista kolmella lapsella tuen tarve', 50),
+    ('60', 'Alueellinen integroitu ryhmä', 'Ryhmän rakenne: VEO/VO, VO ja kaksi LH. Lapsiluku 14 kpl, joista viidellä tuen tarve', 60),
+    ('70', 'Erityisryhmä', 'Sarat-ryhmä. Ryhmän rakenne: VO, SH ja kaksi LH. Lapsiluku 8 kpl, joista kaikilla erityisen tuen tarve', 70),
     ('80', 'Ryhmän pienennyspäätös', 'Tehdään harkinnanvaraisesti', 80),
-    ('90', 'Koululykkäys', 'Lähikoulun rehtorin päätöksellä', 90),
-    ('100', 'Pidennetty oppivelvollisuus', 'Merkittävä, kun lapsella pidennetty oppivelvollisuus.', 100),
-    ('110', 'Harkinnanvarainen kuljetusetu', 'Käytetään silloin kun lapselle on myönnetty harkinnanvarainen kuljetusetu', 110)
-
+    ('90', 'Koululykkäys', 'Lähikoulun rehtorin päätöksellä', 90)
 ON CONFLICT (value) DO
 UPDATE SET
     name_fi = EXCLUDED.name_fi,
@@ -26,3 +22,4 @@ WHERE
     assistance_action_option.name_fi <> EXCLUDED.name_fi OR
     assistance_action_option.description_fi IS DISTINCT FROM EXCLUDED.description_fi OR
     assistance_action_option.display_order <> EXCLUDED.display_order;
+
