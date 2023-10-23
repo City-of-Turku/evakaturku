9e130d48a7f7e313d4544d7c0eb0ae5ab105ae90
Martin Lehtomaa <martin.lehtomaa@rebase.fi>
2023-10-23 16:38:50 +0300

    Fixed footer logo to allign and scale better

--- frontend/turku/citizen.tsx
+++ frontend/turku/citizen.tsx
@@ -7,15 +7,53 @@ SPDX-License-Identifier: LGPL-2.1-or-later
 }
 import React from 'react'
 
+import styled from 'styled-components'
+
+import { desktopMin, tabletMin,  } from 'lib-components/breakpoints'
 import { CitizenCustomizations } from 'lib-customizations/types'
 
 import TurkuLogo from './city-logo-citizen.png'
 import enCustomizations from './enCustomizations'
 import featureFlags from './featureFlags'
 import fiCustomizations from './fiCustomizations'
-import FooterLogo from './footer-logo-citizen.png'
 import mapConfig from './mapConfig'
 import svCustomizations from './svCustomizations'
+
+const TurkuFooterLogo = styled.div`
+  margin-top: 10px;
+  text-align: center;
+
+  @media (min-width: ${tabletMin}) {
+    margin-top: 0;
+    flex-basis: 100%;
+  }
+
+  @media (min-width: ${desktopMin}) {
+    flex-basis: auto;
+    margin-top: -20px;
+    text-align: end;
+  }
+
+  @media (min-width: 1408px) {
+    flex-basis: auto;
+    margin-top: -20px;
+    text-align: end;
+  }
+`
+
+const TurkuFooterImg = styled.img`
+  width: auto;
+
+  @media (min-width: ${desktopMin}) {
+    height: 70px;
+  }
+`
+
+const TurkuFooter = (
+    <TurkuFooterLogo>
+      <TurkuFooterImg src={TurkuLogo} alt="Turku footer logo"/>
+    </TurkuFooterLogo>
+)
 
 const customizations: CitizenCustomizations = {
   appConfig: {},
@@ -29,7 +67,7 @@ const customizations: CitizenCustomizations = {
     src: TurkuLogo,
     alt: 'Turku logo'
   },
-  footerLogo: <img src={FooterLogo} alt="Turku Logo" />,
+  footerLogo: TurkuFooter,
   routeLinkRootUrl: 'https://turku.digitransit.fi/',
   mapConfig,
   featureFlags,
