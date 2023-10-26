494f9fb00c342f5eaa4f37a1df4167901cf7f0cb
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2023-10-26 10:30:07 +0300

    Fix compilation warnings in tests

--- service/src/test/kotlin/fi/turku/evakaturku/payment/service/SapPaymentGeneratorTest.kt
+++ service/src/test/kotlin/fi/turku/evakaturku/payment/service/SapPaymentGeneratorTest.kt
@@ -45,14 +45,13 @@ class SapPaymentGeneratorTest {
     fun `should use full preschool accounting amount when it is not August`() {
         val payment = validPayment()
         val unit = payment.unit.id
-        val payments = listOf(payment)
         whenever(mockFetcher.fetchPreschoolers(any())).thenReturn(mapOf(unit to 10))
         whenever(mockFetcher.fetchPreschoolAccountingAmount(any())).thenReturn(451)
         whenever(mockFetcher.fetchUnitLanguages(any())).thenReturn(mapOf(unit to "fi"))
         whenever(mockIdocGenerator.generate(any(), any(), any())).thenReturn(FIDCCP02.IDOC())
         whenever(mockMarshaller.marshal(any())).thenReturn("XML")
 
-        val result = sapPaymentGenerator.generatePayments(listOf(payment), mockFetcher)
+        sapPaymentGenerator.generatePayments(listOf(payment), mockFetcher)
 
         verify(mockIdocGenerator).generate(payment, 4510, "fi")
     }
@@ -61,14 +60,13 @@ class SapPaymentGeneratorTest {
     fun `should halve the preschool accounting amount and round up in August`() {
         val payment = validPayment().copy(period = DateRange(LocalDate.of(2022, 8, 1), LocalDate.of(2022, 8, 31)))
         val unit = payment.unit.id
-        val payments = listOf(payment)
         whenever(mockFetcher.fetchPreschoolers(any())).thenReturn(mapOf(unit to 10))
         whenever(mockFetcher.fetchPreschoolAccountingAmount(any())).thenReturn(451)
         whenever(mockFetcher.fetchUnitLanguages(any())).thenReturn(mapOf(unit to "fi"))
         whenever(mockIdocGenerator.generate(any(), any(), any())).thenReturn(FIDCCP02.IDOC())
         whenever(mockMarshaller.marshal(any())).thenReturn("XML")
 
-        val result = sapPaymentGenerator.generatePayments(listOf(payment), mockFetcher)
+        sapPaymentGenerator.generatePayments(listOf(payment), mockFetcher)
 
         verify(mockIdocGenerator).generate(payment, 2260, "fi")
     }
