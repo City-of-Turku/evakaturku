42e095f1ede5cac37635ee0e2a68f59f3e538fbf
Mikko Savolainen <mikko.savolainen@gofore.com>
2023-12-08 14:43:00 +0200

    add sns topic finding and notification sending to certificate scheduled task

--- scheduled-tasks/certificate-check/check.sh
+++ scheduled-tasks/certificate-check/check.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+topic=$(aws sns list-topics --output text | grep cert-topic | sed 's/TOPICS//' | tr -d '[:blank:]')
 
-echo foobar
+if [ -n "$topic" ]; then
+  aws sns publish --topic-arn $topic --message "foobar"
+  echo "message sent"
+else
+  echo "no sns topic, message not sent"
+fi
