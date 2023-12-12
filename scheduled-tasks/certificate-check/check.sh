d227f858371599a9161d9fa3e456df22c214f54d
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2023-12-12 12:40:33 +0200

    Get certificate check SNS topic from an environment variable

--- scheduled-tasks/certificate-check/check.sh
+++ scheduled-tasks/certificate-check/check.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-topic=$(aws sns list-topics --output text | grep cert-topic | sed 's/TOPICS//' | tr -d '[:blank:]')
+# topic=$(aws sns list-topics --output text | grep cert-topic | sed 's/TOPICS//' | tr -d '[:blank:]')
 
-if [ -n "$topic" ]; then
-  aws sns publish --topic-arn $topic --message "foobar"
+# if [ -n "$topic" ]; then
+  aws sns publish --topic-arn $SNS_TOPIC_ARN --message "foobar"
   echo "message sent"
-else
-  echo "no sns topic found, message not sent"
-fi
+# else
+#   echo "no sns topic, message not sent"
+# fi
