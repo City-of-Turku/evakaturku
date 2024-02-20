1cf430a2a29ecb302e239636201c2c172ec9b3c1
Martin Lehtomaa <martin.lehtomaa@rebase.fi>
2024-02-20 10:12:09 +0200

    Added calculated child attendance fields to DW reporting

--- scheduled-tasks/reporting/sql/DW-Daily_units_and_groups_attendance.sql
+++ scheduled-tasks/reporting/sql/DW-Daily_units_and_groups_attendance.sql
@@ -1,3 +1,61 @@
+WITH child_attendances AS (
+    SELECT *
+    FROM child_attendance child_att
+    WHERE child_att.date = :date_val::DATE
+),
+default_sn_coefficients AS (
+    SELECT
+        occupancy_coefficient,
+        occupancy_coefficient_under_3y,
+        realized_occupancy_coefficient,
+        realized_occupancy_coefficient_under_3y,
+        valid_placement_type
+    FROM service_need_option
+    WHERE default_option
+),
+daycare_group_placements_on_date as (
+    SELECT
+        dgp.id                                              AS group_placement_id,
+        dgp.daycare_group_id								as group_id,
+        pl.child_id                                         AS child_id,
+        pl.unit_id                                          AS unit_id,
+        CASE
+            WHEN af.capacity_factor IS NOT NULL
+                THEN af.capacity_factor
+            ELSE 1
+        END AS assistance_coefficient,
+        CASE
+            WHEN (u.type && array['FAMILY', 'GROUP_FAMILY']::care_types[]) = TRUE
+                THEN 1.75
+            WHEN :date_val::DATE < (p.date_of_birth + interval '3 year')::DATE
+                THEN (
+                    CASE
+                        WHEN sno.realized_occupancy_coefficient_under_3y IS NOT NULL
+                            THEN sno.realized_occupancy_coefficient_under_3y
+                        ELSE dsc.realized_occupancy_coefficient_under_3y
+                    END
+                )
+            ELSE (
+                CASE
+                    WHEN sno.realized_occupancy_coefficient IS NOT NULL
+                        THEN sno.realized_occupancy_coefficient
+                    ELSE dsc.realized_occupancy_coefficient
+                END
+            )
+        END AS service_need_coefficient
+    FROM daycare_group_placement dgp
+        JOIN placement pl ON dgp.daycare_placement_id = pl.id
+        JOIN daycare u ON u.id = pl.unit_id
+        JOIN person p ON p.id = pl.child_id
+        LEFT JOIN assistance_factor af ON af.child_id = pl.child_id
+            AND af.valid_during @> :date_val::DATE
+        LEFT JOIN service_need sn ON sn.placement_id = pl.id
+            AND daterange(sn.start_date, sn.end_date, '[]') @> :date_val::DATE
+        LEFT JOIN service_need_option sno ON sn.option_id = sno.id
+        JOIN default_sn_coefficients dsc ON dsc.valid_placement_type = pl.type
+    WHERE dgp.start_date <= :date_val::DATE
+        AND :date_val::DATE <= dgp.end_date
+)
 SELECT
     now() AT TIME ZONE 'Europe/Helsinki'       AS aikaleima,
     :date_val::DATE 						   AS poiminta_ajalta_pvm,
@@ -24,46 +82,49 @@ SELECT
         CASE
             WHEN sar.arrived IS NOT NULL
                 THEN ROUND(
-                                EXTRACT(
-                                        EPOCH FROM (
-                                        LEAST(sar.departed, timezone('Europe/Helsinki', (:date_val::DATE + interval '1' day)::date::timestamp)) -
-                                        GREATEST(sar.arrived, timezone('Europe/Helsinki', :date_val::DATE::timestamp))
-                                    )
-                                    ) / 3600 / 7.75 * sar.occupancy_coefficient / 7,
-                                4
+                    EXTRACT(
+                        EPOCH FROM (
+                            LEAST(sar.departed, timezone('Europe/Helsinki', (:date_val::DATE + interval '1' day)::date::timestamp)) -
+                            GREATEST(sar.arrived, timezone('Europe/Helsinki', :date_val::DATE::timestamp))
+                        )
+                    ) / 3600 / 7.65 * sar.occupancy_coefficient / 7, 4
                 )
             ELSE sa.count
             END
     )                                      AS henkilökuntaa_läsnä,
     (
-        SELECT count(*)
-		FROM child_attendance child_att
-		WHERE child_att.date = :date_val::DATE
-		    AND child_att.unit_id = d.id
+        SELECT count(distinct child_att.child_id)
+		FROM child_attendances child_att
+		WHERE child_att.unit_id = d.id
             AND EXISTS (
                 SELECT *
-                FROM daycare_group_placement dgp2
-                    JOIN placement p2 on p2.id = dgp2.daycare_placement_id
-                WHERE dgp2.daycare_group_id = dg.id
-                    AND p2.child_id = child_att.child_id
-                    AND dgp2.start_date <= :date_val::DATE
-                    AND :date_val::DATE <= dgp2.end_date)
+                FROM daycare_group_placements_on_date dgpod
+                WHERE dgpod.group_id = dg.id
+                    AND dgpod.child_id = child_att.child_id)
     )                                      AS lapsia_läsnä_ryhmässä,
     (
-        SELECT COUNT(*)
-        FROM child_attendance child_att
-        WHERE child_att.date = :date_val::DATE AND child_att.unit_id = d.id
+    	SELECT SUM(dgpod.assistance_coefficient * dgpod.service_need_coefficient)
+		FROM child_attendances child_att
+			JOIN daycare_group_placements_on_date dgpod ON dgpod.child_id = child_att.child_id
+		WHERE child_att.unit_id = d.id AND dgpod.group_id = dg.id
+    )                                      AS laskennallinen_lapsia_läsnä_ryhmässä,
+    (
+        SELECT COUNT(distinct child_att.child_id)
+        FROM child_attendances child_att
+        WHERE child_att.unit_id = d.id
     )                                      AS lapsia_läsnä_yksikössä,
     (
-        SELECT count(distinct p.child_id)
-        FROM daycare_group_placement dgp
-            JOIN placement p ON dgp.daycare_placement_id = p.id
-        WHERE dgp.daycare_group_id = dg.id
-            AND dgp.start_date <= :date_val::DATE
-            AND :date_val::DATE <= dgp.end_date
+    	SELECT COUNT(distinct dgpod.child_id)
+        FROM daycare_group_placements_on_date dgpod
+        WHERE dgpod.group_id = dg.id
     )                                      AS ryhmän_lapsimäärä,
     (
-        SELECT count(distinct p.child_id)
+        SELECT SUM(dgpod.assistance_coefficient * dgpod.service_need_coefficient)
+        FROM daycare_group_placements_on_date dgpod
+        WHERE dgpod.group_id = dg.id
+    )                                      AS laskennallinen_ryhmän_lapsimäärä,
+    (
+        SELECT COUNT(distinct p.child_id)
         FROM daycare_group_placement dgp
             JOIN placement p ON dgp.daycare_placement_id = p.id
         WHERE dgp.daycare_group_id = dg.id
@@ -82,14 +143,14 @@ FROM daycare_group dg
         SELECT group_id, arrived, departed, occupancy_coefficient
         FROM staff_attendance_realtime
         WHERE departed IS NOT NULL
-            AND type NOT IN ('OTHER_WORK', 'TRAINING')
+        AND type NOT IN ('OTHER_WORK', 'TRAINING')
         UNION ALL
         SELECT group_id, arrived, departed, occupancy_coefficient
         FROM staff_attendance_external
         WHERE departed IS NOT NULL
     ) sar ON sar.group_id = dg.id AND (date(arrived) = :date_val::DATE OR date(departed) = :date_val::DATE)
 WHERE (:date_val::DATE - interval '3 months' <= d.closing_date OR d.closing_date is null)
-    AND (:date_val::DATE - interval '3 months' <= dg.end_date OR dg.end_date is null)
+  AND (:date_val::DATE - interval '3 months' <= dg.end_date OR dg.end_date is null)
 GROUP BY
     d.name,
     d.id,
