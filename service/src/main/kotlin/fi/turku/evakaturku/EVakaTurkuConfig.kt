8f0aae481650739fad3bec16aa2088229577dd02
tahtee <96183234+tahtee@users.noreply.github.com>
2025-04-04 07:44:15 +0300

    Fixed build: added skipGuardianApproval and set skipGuardianApproval enabled

--- service/src/main/kotlin/fi/turku/evakaturku/EVakaTurkuConfig.kt
+++ service/src/main/kotlin/fi/turku/evakaturku/EVakaTurkuConfig.kt
@@ -50,6 +50,7 @@ class EVakaTurkuConfig {
             municipalMessageAccountName = "Turun kaupunki",
             serviceWorkerMessageAccountName = "Turun kaupunki",
             applyPlacementUnitFromDecision = false,
+            skipGuardianPreschoolDecisionApproval = true,
             fiveYearsOldDaycareEnabled = env.lookup("evaka.five_years_old_daycare.enabled") ?: false,
             financeMessageAccountName =
                 "Varhaiskasvatuksen asiakasmaksut - Småbarnspedagogikens avgifter - Early childhood education fees",
