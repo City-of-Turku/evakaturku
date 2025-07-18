c37b8e899d2fd7a9406eee070388c1d8dbc2b6d7
Mikko Savolainen <mikko.savolainen@gofore.com>
2025-07-18 15:34:03 +0300

    boilerplate, queries, data classes, initial test implementation

---
+++ service/src/main/kotlin/fi/turku/evakaturku/dw/DWExportJob.kt
@@ -0,0 +1,32 @@
+package fi.turku.evakaturku.dw
+
+import fi.espoo.evaka.espoo.bi.CSV_CHARSET
+import fi.espoo.evaka.espoo.bi.EspooBiJob
+import fi.espoo.evaka.shared.db.Database
+import fi.espoo.evaka.shared.domain.EvakaClock
+import fi.turku.evakaturku.EvakaTurkuAsyncJob
+import java.time.Duration
+
+class DWExportJob(private val client: DWExportClient) {
+    fun sendDWQuery(
+        db: Database.Connection,
+        clock: EvakaClock,
+        msg: EvakaTurkuAsyncJob.SendDWQuery,
+    ) = sendDWQuery(db, clock, msg.query.queryName, msg.query.query)
+
+    fun sendDWQuery(
+        db: Database.Connection,
+        clock: EvakaClock,
+        queryName: String,
+        query: DWQueries.CsvQuery,
+    ) {
+        db.read { tx ->
+            tx.setStatementTimeout(Duration.ofMinutes(10))
+
+            query(tx) { records ->
+                val stream = EspooBiJob.CsvInputStream(CSV_CHARSET, records)
+                client.sendDWCsvFile(queryName, clock, stream)
+            }
+        }
+    }
+}
--- service/src/test/kotlin/fi/turku/evakaturku/IntegrationTestConfiguration.kt
+++ service/src/test/kotlin/fi/turku/evakaturku/IntegrationTestConfiguration.kt
@@ -6,12 +6,16 @@ package fi.turku.evakaturku
 
 import com.auth0.jwt.algorithms.Algorithm
 import fi.espoo.evaka.BucketEnv
+import fi.turku.evakaturku.invoice.service.SftpConnector
+import fi.turku.evakaturku.invoice.service.SftpSender
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.core.internal.http.loader.DefaultSdkHttpClientBuilder
 import software.amazon.awssdk.http.SdkHttpConfigurationOption
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.S3Configuration
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
@@ -31,6 +35,7 @@ class IntegrationTestConfiguration {
         val client =
             S3Client.builder()
                 .httpClient(DefaultSdkHttpClientBuilder().buildWithDefaults(attrs))
+                .region(Region.US_WEST_1)
                 .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
                 .endpointOverride(bucketEnv.localS3Url)
                 .credentialsProvider(
@@ -49,6 +54,20 @@ class IntegrationTestConfiguration {
     }
 
     @Bean
+    fun testS3AsyncClient(bucketEnv: BucketEnv): S3AsyncClient =
+        S3AsyncClient.crtBuilder()
+            .httpConfiguration { it.trustAllCertificatesEnabled(true) }
+            .region(Region.EU_WEST_1)
+            .forcePathStyle(true)
+            .endpointOverride(bucketEnv.localS3Url)
+            .credentialsProvider(
+                StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(bucketEnv.localS3AccessKeyId, bucketEnv.localS3SecretAccessKey),
+                ),
+            )
+            .build()
+
+    @Bean
     fun s3Presigner(bucketEnv: BucketEnv): S3Presigner =
         S3Presigner.builder()
             .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
@@ -59,6 +78,12 @@ class IntegrationTestConfiguration {
                 ),
             )
             .build()
+
+    @Bean
+    fun sftpSender(
+        properties: EvakaTurkuProperties,
+        connector: SftpConnector,
+    ): SftpSender = SftpSender(properties.dwExport.sftp, connector)
 
     @Bean
     fun jwtAlgorithm(): Algorithm {
